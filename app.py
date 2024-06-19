6ca213619b5fd7473146ca94a5a67d0c7f0fe0a0
walideker <walidadr95@gmail.com>
2024-06-19 21:45:10 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,59 @@
+patient='john'
+age= 20
+is_new= True 
+
+print('hello world')
+
+# receiving input 
+
+name = input("what is ur name? ")
+print (name  )
+
+birth_year = input('enter ur birthyear: ')
+age = 2024 - int(birth_year)
+print(  age)
+
+First = input("First: ")
+Second = input("Second: ")
+Sum = int(First)+int(Second)
+print(Sum)
+
+looser = ('Iam a looser')
+var = ('looser' in looser)
+print(var )
+
+price = 8
+print(not  price > 3)
+#  not operaton inverse the boolean 
+# functions 
+
+temp = 20 
+if temp> 27 :
+    print('its hot outside')
+elif temp == 20 :
+    print('its a normal day ')
+else : print('wear a hoddy')
+
+weight = input('weight: ')
+unit = input('k or L: ')
+
+if unit == k:
+    print
+i = 5
+while i <= 10:
+    print(i)
+    i = i + 1
+names = ['walid','mom','pechpouch','lina']
+for name in names:
+    print(name)
+
+numbers = (1,2,3)
+numbers[0]= 1
+
+# tuples r ummutable means u cant change the value  
+
+
+
+
+# print('walid' in names )
+# in this example the python will read the first 3 but exclude the nubmer 3
